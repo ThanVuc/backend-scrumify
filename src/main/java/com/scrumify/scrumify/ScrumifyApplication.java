d1af9843e3634fde4664ff06913993aca3e16f73
sinh <sinhhahaha1@gmail.com>
2026-07-31 21:39:41 +0700

    init: init project structure

---
+++ src/main/java/com/scrumify/scrumify/ScrumifyApplication.java
@@ -0,0 +1,13 @@
+package com.scrumify.scrumify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scrumify/scrumify/ScrumifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrumify.scrumify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
