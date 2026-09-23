06be7734f3accbc8a3f8a30b445b3f26d72f54b3
sinh <sinhnguyen417@gmail.com>
2026-09-23 22:58:03 +0700

    feat(auth): add cors config

--- src/main/java/com/scrumify/scrumify/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/scrumify/scrumify/infrastructure/security/SecurityConfig.java
@@ -4,6 +4,9 @@ import com.scrumify.scrumify.api.filter.JwtAuthenticationFilter;
 import com.scrumify.scrumify.api.filter.TraceIdFilter;
 import com.scrumify.scrumify.api.security.RestAccessDeniedHandler;
 import com.scrumify.scrumify.api.security.RestAuthenticationEntryPoint;
+
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
@@ -16,6 +19,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -32,6 +38,7 @@ public class SecurityConfig {
                         RestAccessDeniedHandler accessDeniedHandler, TraceIdFilter traceIdFilter) throws Exception {
                 http
                                 .csrf(csrf -> csrf.disable())
+                                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                                 .authorizeHttpRequests(auth -> auth
                                                 .anyRequest().permitAll())
                                 .exceptionHandling(ex -> ex
@@ -55,4 +62,27 @@ public class SecurityConfig {
                                     ROLE_ADMIN > ROLE_MEMBER
                                 """);
         }
+
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration configuration = new CorsConfiguration();
+
+                configuration.setAllowedOrigins(List.of(
+                                "http://localhost:5173",
+                                "http://scrumify-sandbox:5000"
+                        ));
+
+                configuration.setAllowedMethods(List.of(
+                                "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+
+                configuration.setAllowedHeaders(List.of("*"));
+                configuration.setAllowCredentials(true);
+
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+                source.registerCorsConfiguration("/**", configuration);
+
+                return source;
+        }
+
 }
